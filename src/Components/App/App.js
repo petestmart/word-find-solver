711a816c04970a4fb105120f815333c6dd9815e3
Pete St. Martin <peter.r.stmartin@gmail.com>
2019-09-25 13:54:53 -0500

    WordFinder component added. Initial Setup.

---
+++ src/Components/App/App.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import './App.css';
+import WordFind from '../WordFind/WordFind';
+
+class App extends Component {
+  render() {
+
+  
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1>Word Find Solver</h1>
+        </header>
+        <body>
+          <WordFind />
+        </body>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/Components/WordFind/WordFind.jsx
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+
+class WordFind extends Component {
+    render() {
+        return (
+            <p>
+                A T L L F U V D E Y O B Z V D
+                W F B N E D X G H E A N P O R
+                O T V B Y L A L G T D K E A A
+                D O O W D R A H H E L A S P Z
+                A P P E A K H R O F X W L X O
+                W B R G A S O M M B R O K E R
+                M C X G X O U I E O K M Y K W
+                A O E F M R S L S N L R S I I
+                S N P D B C E Q P R I U K U Q
+                T E G R I P E B O Q U I Q S C
+                B P A S D Q P E T X J P S E S
+                B R K R R E U E T T D Z D K L
+                L B J B C B B L E U B I U R F
+                L N H S F H T K R K G H Y A M
+                O J H D N Q A J S Q P L R M U
+            </p>
+        )
+    }
+}
+
+export default WordFind;
