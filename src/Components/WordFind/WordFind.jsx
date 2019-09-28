fc9080e230683ac487d5988f4a2fbfb4dc0e9a63
Pete St. Martin <peter.r.stmartin@gmail.com>
2019-09-28 10:40:46 -0500

    User input of wordfind is functional for any wordfind

--- src/Components/WordFind/WordFind.jsx
+++ src/Components/WordFind/WordFind.jsx
@@ -10,16 +10,26 @@ import WordOutput from './WordOutput';
 
 // ========== STYLES ========== //
 import Button from '@material-ui/core/Button';
+import TextField from '@material-ui/core/TextField';
 import './WordFind.css'
 
 class WordFind extends Component {
 
     state = {
         displayPuzzle: false,
-
+        puzzleInput: 'initial',
     }
 
-    wordsMatch = [];
+    wordsMatch;
+
+    handleChange = (event) => {
+        console.log('Input Word Find', event.target.value);
+        let puzzleInput = event.target.value
+        this.setState({
+            puzzleInput: puzzleInput
+        })
+        console.log('this.state.puzzleInput', this.state.puzzleInput);
+    }
     
     puzzleDisplay = (input) => {
 
@@ -153,7 +163,8 @@ class WordFind extends Component {
 
     render() {
 
-        let puzzleData = "A T L L F U V D E Y O B Z V D W F B N E D X G H E A N P O R O T V B Y L A L G T D K E A A D O O W D R A H H E L A S P Z A P P E A K H R O F X W L X O W B R G A S O M M B R O K E R M C X G X O U I E O K M Y K W A O E F M R S L S N L R S I I S N P D B C E Q P R I U K U Q T E G R I P E B O Q U I Q S C B P A S D Q P E T X J P S E S B R K R R E U E T T D Z D K L L B J B C B B L E U B I U R F L N H S F H T K R K G H Y A M O J H D N Q A J S Q P L R M U";
+        // let puzzleData = "A T L L F U V D E Y O B Z V D W F B N E D X G H E A N P O R O T V B Y L A L G T D K E A A D O O W D R A H H E L A S P Z A P P E A K H R O F X W L X O W B R G A S O M M B R O K E R M C X G X O U I E O K M Y K W A O E F M R S L S N L R S I I S N P D B C E Q P R I U K U Q T E G R I P E B O Q U I Q S C B P A S D Q P E T X J P S E S B R K R R E U E T T D Z D K L L B J B C B B L E U B I U R F L N H S F H T K R K G H Y A M O J H D N Q A J S Q P L R M U";
+        let puzzleData = this.state.puzzleInput;
         let puzzle = puzzleData.split(' ');
 
 
@@ -161,10 +172,18 @@ class WordFind extends Component {
         if (this.state.displayPuzzle === false) {
             return (
                 <p>
+                    <TextField
+                        onChange={this.handleChange}
+                        id="word-find-input"
+                        label="Enter Word Find Input"
+                        multiline
+                        rows="4"
+                        margin="normal"
+                    />
                     <Button
                         onClick={() => this.puzzleDisplay(puzzle)}
 
-                    >Run Program</Button>
+                    >Find Words</Button>
                     {/* <Button
                         onClick={() => this.sendToRedux()}
                     >Redux Button</Button> */}
